9648a3daec6f821ec284e33a47b94bdd711bb670
Jane Kim <jane.kim0717@gmail.com>
2022-03-23 17:39:42 -0400

    Completed Core Deliverables

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,14 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        one_freebie = self.freebies.find_by(item_name: item_name) 
+        one_freebie ? true : false
+    end
+
+    def give_away(dev, freebie)
+        freebie.update(dev: dev) unless self != freebie.dev
+    end
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,5 +17,10 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+Freebie.create(item_name: "keychain", value: 1, dev_id: 1, company_id: 1)
+Freebie.create(item_name: "pen", value: 2, dev_id: 2, company_id: 1)
+Freebie.create(item_name: "tshirt", value: 1, dev_id: 3, company_id: 2)
+Freebie.create(item_name: "notebook", value: 3, dev_id: 4, company_id: 3)
+Freebie.create(item_name: "mug", value: 4, dev_id: 1, company_id: 4)
 
 puts "Seeding done!"
